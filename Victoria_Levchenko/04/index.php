7d9c87a424759567e7e21080742347d96b82ba84
Victoria Levchenko <victoria.s.levchenko@gmail.com>
2016-05-07 14:50:47 +0300

    LevchenkoVika.Homeworks3,4

--- Victoria_Levchenko/04/index.php
+++ Victoria_Levchenko/04/index.php
@@ -1,39 +1,42 @@
 <!doctype html>
- <head>
- <meta charset="UTF-8">
- <title>lesson 4</title>
- <style>
-  table {
-   margin-bottom: 50px;
-  }
-   th {
-    background: navy;
-    color: white;
-   }
-   td {
-    text-align: center;
-    border: 1px solid navy;
-   }
- </style>
- </head>
- <body>
-<?php
-$cols = 10;
-$rows = 10;
+<head>
+    <meta charset="UTF-8">
+    <title>lesson 4</title>
+    <style>
+        table {
+            margin-bottom: 50px;
+        }
 
-echo '<table>';
-for ($a=1; $a<=$rows; $a++) {
-  echo '<tr>';
- for ($b = 1; $b <= $cols; $b++) {
-  if ($a==1 || $b==1) {
-   echo '<th>'. $a * $b. '</th>';
-  } else {
-  echo '<td>'. $a * $b. '</td>'; }
- }
- echo '</tr>';
-}
- echo '</table>';
- ?>
+        th {
+            background: navy;
+            color: white;
+        }
 
- </body>
+        td {
+            text-align: center;
+            border: 1px solid navy;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <?php
+    $cols = 10;
+    $rows = 10;
+
+    for ($a = 1; $a <= $rows; $a++) {
+        echo '<tr>';
+        for ($b = 1; $b <= $cols; $b++) {
+            if ($a == 1 || $b == 1) {
+                echo '<th>' . $a * $b . '</th>';
+            } else {
+                echo '<td>' . $a * $b . '</td>';
+            }
+        }
+        echo '</tr>';
+    }
+    ?>
+</table>
+
+</body>
 </html>
